bdda6d99d7afa1c53d06f7cf8fa3d0a9d07f2dfe
ttejeda02 <ttejeda.rguez@gmail.com>
2024-09-25 20:50:20 -0600

    add: checkTotalPricesInCheckout to test prices subtotal and total

--- src/test/java/test/PriceCalculationTest.java
+++ src/test/java/test/PriceCalculationTest.java
@@ -7,6 +7,9 @@ import model.User;
 import pages.*;
 import service.UserCreator;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class PriceCalculationTest extends BaseTest {
 
     @Test (priority = 1)
@@ -24,5 +27,38 @@ public class PriceCalculationTest extends BaseTest {
         Double[] actualProductPrices = cartPage.getProductsPrices();
 
         Assert.assertArrayEquals(expected, actualProductPrices);
+    }
+
+    @Test (priority = 3)
+    public void checkTotalPricesInCheckout() {
+        User user = UserCreator.createStandardUserWithUserData();
+        UserActionsUtil.login(driver, user);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        UserActionsUtil.saveThreeProducts(driver);
+        inventoryPage.goToTheCartPage();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.goToCheckoutPage();
+
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.fillUserFormInput(user);
+        checkoutPage.goToCheckoutOverviewPage();
+
+        CheckoutOverviewPage checkoutOverviewPage = new CheckoutOverviewPage(driver);
+
+        Double expectedSubTotalPrice = 81.97;
+        Double actualSubTotalPrice = checkoutOverviewPage.getTotalPriceWithoutTaxes();
+        Assert.assertEquals(expectedSubTotalPrice, actualSubTotalPrice);
+
+        Double expectedTotalPrice = calculatePriceWithTaxes(expectedSubTotalPrice);
+        Double actualTotalPrice = checkoutOverviewPage.getTotalPriceWithTaxes();
+        Assert.assertEquals(expectedTotalPrice, actualTotalPrice);
+    }
+
+    private Double calculatePriceWithTaxes(Double taxes) {
+        BigDecimal expectedTotalPrice = BigDecimal.valueOf(taxes + (taxes * 0.08));
+        expectedTotalPrice = expectedTotalPrice.setScale(2, RoundingMode.UP);
+        return expectedTotalPrice.doubleValue();
     }
 }
