126bf0839a61c5dc19fe45cf7379c6ad9a4cecb0
ttejeda02 <ttejeda.rguez@gmail.com>
2024-10-16 16:18:30 -0600

    create: negative tests

--- src/test/java/test/FailAddProductTest.java
+++ src/test/java/test/FailAddProductTest.java
@@ -1,4 +1,46 @@
 package test;
 
-public class FailAddProductTest {
+import model.User;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.InventoryPage;
+import service.UserCreator;
+
+public class FailAddProductTest extends BaseTest{
+
+    @Test (priority = 1)
+    public void failAddAProduct(){
+        User user = UserCreator.createErrorUserWithUserData();
+        UserActionsUtil.login(driver, user);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.clickAddToCartSauceLabsBolt();
+        inventoryPage.clickAddToCartSauceLabsBackpack();
+        inventoryPage.clickAddToCartTestAllthingsTShirt();
+        inventoryPage.goToTheCartPage();
+
+        CartPage cartPage = new CartPage(driver);
+        String[] expectedProductsInCar = {"Sauce Labs Backpack"};
+        String[] actualProductsInCar = cartPage.getProductInCart();
+
+        Assert.assertArrayEquals(expectedProductsInCar, actualProductsInCar);
+    }
+
+    @Test (priority = 2)
+    public void failAddThreeProducts(){
+        User user = UserCreator.createErrorUserWithUserData();
+        UserActionsUtil.login(driver, user);
+        UserActionsUtil.saveThreeProducts(driver);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.goToTheCartPage();
+
+        CartPage cartPage = new CartPage(driver);
+        String[] expectedProductsInCar = {};
+        String[] actualProductsInCar = cartPage.getProductInCart();
+
+        Assert.assertArrayEquals(expectedProductsInCar, actualProductsInCar);
+    }
+
 }
