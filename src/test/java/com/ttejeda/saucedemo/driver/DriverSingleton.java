f1f19c06f7d5d481a4d7fe6ab04b8b3493e41d88
ttejeda02 <ttejeda.rguez@gmail.com>
2024-10-18 18:12:53 -0600

    add: documentation for DriverSingleton

--- src/test/java/com/ttejeda/saucedemo/driver/DriverSingleton.java
+++ src/test/java/com/ttejeda/saucedemo/driver/DriverSingleton.java
@@ -9,12 +9,24 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+
+/**
+ * Singleton class to manage WebDriver instance for browser automation.
+ */
 public class DriverSingleton {
     private static WebDriver driver;
 
+    /**
+     * Retrieves the WebDriver instance. If the instance is not already initialized (null),
+     * it initializes it based on the "browser" system property (-Dbrowser flag).
+     * If the "browser" property is not provided, Chrome will be used as the default browser.
+     *
+     * @return WebDriver instance
+     */
     public static WebDriver getDriver() {
         if (driver == null){
-            switch (System.getProperty("browser")) {
+            String browser = System.getProperty("browser", "chrome");
+            switch (browser.toLowerCase()) {
                 case "chrome": {
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
@@ -53,9 +65,13 @@ public class DriverSingleton {
                 }
             }
         }
+
         return driver;
     }
 
+    /**
+     * Quits the WebDriver instance and sets it to null
+     */
     public static void quitDriver() {
         driver.quit();
         driver = null;
