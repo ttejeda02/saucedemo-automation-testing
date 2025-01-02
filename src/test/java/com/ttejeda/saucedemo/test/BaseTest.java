226827307d9a2c04089eeb8a1b513396143e0b06
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-01 23:15:11 -0600

    add: class documentation

--- src/test/java/com/ttejeda/saucedemo/test/BaseTest.java
+++ src/test/java/com/ttejeda/saucedemo/test/BaseTest.java
@@ -5,14 +5,41 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
+/**
+ * BaseTest class provides the foundation for all test classes.
+ * It contains the common setup and teardown methods that are executed
+ * before and after each test method.
+ * <p>
+ * This class is responsible for initializing the WebDriver before each test
+ * and cleaning up after each test by quitting the WebDriver instance.
+ * </p>
+ */
+
 public class BaseTest {
     protected WebDriver driver;
 
+    /**
+     * Initializes the WebDriver before each test method.
+     * <p>
+     * This method calls {@link DriverSingleton#getDriver()} to obtain a new
+     * instance of the WebDriver. It runs before the execution of each test
+     * method to ensure that a fresh WebDriver instance is available.
+     * </p>
+     */
     @BeforeMethod(alwaysRun = true)
     public void setUp(){
         driver = DriverSingleton.getDriver();
     }
 
+    /**
+     * Quits the WebDriver and removes the instance after each test method.
+     * <p>
+     * This method calls {@link DriverSingleton#quitDriver()} to close the browser
+     * and properly clean up the WebDriver instance, ensuring there are no lingering
+     * processes after each test. It runs after the execution of each test method.
+     * </p>
+     */
     @AfterMethod(alwaysRun = true)
     public void quitBrowser(){
         DriverSingleton.quitDriver();
