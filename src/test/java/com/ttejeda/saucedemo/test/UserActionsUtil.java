3fd71937ddf4405e2f124f52912fb6c1f0c335b4
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-02 22:27:05 -0600

    add: class documentation

--- src/test/java/com/ttejeda/saucedemo/test/UserActionsUtil.java
+++ src/test/java/com/ttejeda/saucedemo/test/UserActionsUtil.java
@@ -5,12 +5,29 @@ import com.ttejeda.saucedemo.pages.InventoryPage;
 import org.openqa.selenium.WebDriver;
 import com.ttejeda.saucedemo.pages.LoginPage;
 
+
+/**
+ * UserActionsUtil class provides basic actions that can be performed
+ * on the Saucedemo page
+ */
 public class UserActionsUtil {
+
+    /**
+     * Logs in to the Saucedemo page using the provided user credentials.
+     *
+     * @param driver the WebDriver instance for interacting with the browser
+     * @param user the User object containing the user's credentials
+     */
     public static void login(WebDriver driver, User user) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.accountLogin(user);
     }
 
+    /**
+     * Adds three products to the cart from the inventory page.
+     *
+     * @param driver the WebDriver instance for interacting with the browser
+     */
     public static void saveThreeProducts(WebDriver driver) {
         InventoryPage inventoryPage = new InventoryPage(driver);
         inventoryPage.clickAddToCartSauceLabsBolt();
