84014a5082ce7ff23ef3239a2e96d424c0938eb7
ttejeda02 <ttejeda.rguez@gmail.com>
2025-01-05 23:22:55 -0600

    add: CheckoutOverwiewPage documentation

--- src/test/java/com/ttejeda/saucedemo/pages/CheckoutOverviewPage.java
+++ src/test/java/com/ttejeda/saucedemo/pages/CheckoutOverviewPage.java
@@ -7,30 +7,54 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class CheckoutOverviewPage {
 
+/**
+ * This class represents the Checkout Overview page in the purchasing flow of the Saucedemo application.
+ * It contains WebElements for displaying the subtotal, total price, product prices,
+ * and buttons to finish or cancel the checkout process.
+ * The class provides methods to retrieve price information and interact with the buttons to complete
+ * or cancel the checkout.
+ */
+public class CheckoutOverviewPage {
+    // Webdriver instance
     WebDriver driver;
 
-    @FindBy (className = "summary_subtotal_label")
-    WebElement subtotal;
+    // WebElement for the subtotal of the order
+    @FindBy(className = "summary_subtotal_label")
+    private WebElement subtotal;
 
-    @FindBy (xpath = "//div[starts-with(text(), 'Total')]")
-    WebElement total;
+    // WebElement for the total price, including taxes
+    @FindBy(xpath = "//div[starts-with(text(), 'Total')]")
+    private WebElement total;
 
-    @FindBy (id = "finish")
-    WebElement finishCheckoutButton;
+    // WebElement for the finish checkout button
+    @FindBy(id = "finish")
+    private WebElement finishCheckoutButton;
 
-    @FindBy (id = "cancel")
-    WebElement cancelCheckoutButton;
+    // WebElement for the cancel checkout button
+    @FindBy(id = "cancel")
+    private WebElement cancelCheckoutButton;
 
-    @FindBy (className = "inventory_item_price")
-    List<WebElement> priceList;
+    // List of WebElements representing the prices of the products in the order
+    @FindBy(className = "inventory_item_price")
+    private List<WebElement> priceList;
 
+    /**
+     * Constructor that initializes the CheckoutOverviewPage with the provided WebDriver.
+     * It initializes the page elements using PageFactory.
+     *
+     * @param driver The WebDriver instance used to interact with the web browser.
+     */
     public CheckoutOverviewPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(this.driver, this);
+        PageFactory.initElements(this.driver, this); // Initialize the WebElements
     }
 
+    /**
+     * Retrieves the total price of the order without taxes.
+     *
+     * @return The subtotal as a Double value.
+     */
     public Double getTotalPriceWithoutTaxes() {
         String subtotalText = subtotal.getText();
         subtotalText = subtotalText.replace("Item total: $", "");
@@ -38,6 +62,11 @@ public class CheckoutOverviewPage {
         return Double.parseDouble(subtotalText);
     }
 
+    /**
+     * Retrieves the total price of the order including taxes.
+     *
+     * @return The total price with taxes as a Double value.
+     */
     public Double getTotalPriceWithTaxes() {
         String totalText = total.getText();
         totalText = totalText.replace("Total: $", "");
@@ -45,14 +74,26 @@ public class CheckoutOverviewPage {
         return Double.parseDouble(totalText);
     }
 
+    /**
+     * Completes the checkout process by clicking the "Finish" button.
+     */
     public void finishCheckout() {
         finishCheckoutButton.click();
     }
 
+    /**
+     * Cancels the checkout process and returns to the Inventory page by clicking the "Cancel" button.
+     */
     public void cancelCheckoutAndReturnToInventoryPage() {
         cancelCheckoutButton.click();
     }
 
+    /**
+     * Retrieves the prices of the products in the order.
+     * The price values are parsed from string format (e.g., "$29.99") into Double.
+     *
+     * @return An array of Doubles representing the prices of the products in the order.
+     */
     public Double[] getProductsPrices() {
         String[] productPricesStrings = new String[priceList.size()];
         for (int i = 0; i < productPricesStrings.length; i++) {
